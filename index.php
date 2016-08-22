42b0d0c9caa15e860a4ab886282554557fe64706
Bram Geelen <geelen.bram@hotmail.com>
2016-08-22 11:36:26 +0200

    remove conflicts

--- index.php
+++ index.php
@@ -11,11 +11,7 @@ $query1 = "SELECT full_name, bier.*, jaar " .
 
 $q1 = $mysqli->query($query1);
 
-<<<<<<< HEAD
 $query2 = "SELECT users.uid, jaar, full_name, picture, female, (bier.cola + bier.bier + 2 * bier.duvel) as score " .
-=======
-$query2 = "SELECT users.uid, jaar, full_name, picture, (bier.cola + bier.bier + 2 * bier.duvel) as score " .
->>>>>>> dc22dcab24d7411503d8fc67e3cc9c700bc28112
           "FROM `bier` " .
           "INNER JOIN users ON users.uid = bier.uid " .
           "INNER JOIN users_groups on users_groups.uid = bier.uid " .
