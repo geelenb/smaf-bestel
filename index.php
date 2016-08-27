3676125ef2a11b81c2039d1b8f27e2da6099d109
Bram Geelen <geelen.bram@hotmail.com>
2016-08-27 16:26:25 +0200

    iOS tweaks

--- index.php
+++ index.php
@@ -77,16 +77,15 @@ function convert_utf8( $string ) {
 		<meta name="apple-mobile-web-app-capable" content="yes">
 		<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
 		<meta name="apple-mobile-web-app-title" content="SMAF Bier">
-		<link rel="apple-touch-icon" href="img/touch/apple-touch-icon.png">
-		<link rel="apple-touch-icon" href="touch-icon-iphone.png">
-		
-		<link rel="apple-touch-icon" sizes="76x76" href="touch-icon-ipad.png">
-		<link rel="apple-touch-icon" sizes="120x120" href="touch-icon-iphone-retina.png">
-		<link rel="apple-touch-icon" sizes="152x152" href="touch-icon-ipad-retina.png">
-		<link rel="apple-touch-startup-image" href="/startup.png">
+
+		<link rel="apple-touch-icon" sizes="76x76" href="img/touch/touch-icon-ipad.png">
+		<link rel="apple-touch-icon" sizes="120x120" href="img/touch/touch-icon-iphone-retina.png">
+		<link rel="apple-touch-icon" sizes="152x152" href="img/touch/touch-icon-ipad-retina.png">
+		<link rel="apple-touch-icon" sizes="2048x2048" href="img/touch/apple-touch-icon.png">
+		<link rel="apple-touch-startup-image" href="img/touch/startup.png">
 
 		<meta name="msapplication-TileImage" content="img/touch/ms-touch-icon-144x144-precomposed.png">
-		<meta name="msapplication-TileColor" content="#ffc107">
+		<meta name="msapplication-TileColor" content="#ffd400">
 
 		<!-- normale boel -->
 		<link rel="stylesheet" href="./material.min.css" />
